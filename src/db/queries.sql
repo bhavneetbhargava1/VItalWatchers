f5ca81c525997173e165b65c54e6cb5ffe62e53f
Binal Dhaliwal <binald@uw.edu>
2024-11-01 15:55:40 -0700

    Query 7

--- src/db/queries.sql
+++ src/db/queries.sql
@@ -33,3 +33,55 @@ WHERE EXISTS (
       AND hs.Vital_signs = 'Critical'
 );
 
+-- Query 7: Create non-trivial SQL query that uses two tables in FROM clause
+-- Purpose: This query retrieves a list of patients who have received recent alerts,
+--          along with details about the specific alert and the associated provider.
+-- Summary of Result: It shows the Patient's name, alert type, time of the alert, provider's name,
+--          and any associated notes for monitoring purposes.
+SELECT
+    PATIENTS.First_name AS Patient_First_Name,
+    PATIENTS.Last_name AS Patient_Last_Name,
+    ALERTS.Alert_type AS Alert_Type,
+    ALERTS.Time_stamp AS Alert_Time,
+    PROVIDERS.First_name AS Provider_First_Name,
+    PROVIDERS.Last_name AS Provider_Last_Name,
+    HEALTH_SUMMARY.Provider_notes AS Provider_Notes
+FROM
+    PATIENTS
+        JOIN
+    ALERTS ON PATIENTS.Patient_ID = ALERTS.Patient_ID
+        JOIN
+    HEALTH_SUMMARY ON PATIENTS.Patient_ID = HEALTH_SUMMARY.Patient_ID
+        JOIN
+    PROVIDERS ON HEALTH_SUMMARY.Provider_ID = PROVIDERS.Provider_ID
+WHERE
+    ALERTS.Resolved = 'F';
+
+
+
+-- Query 7: Create non-trivial SQL query that uses two tables in FROM clause
+-- Purpose: This query retrieves a list of patients who have received recent alerts,
+--          along with details about the specific alert and the associated provider.
+-- Summary of Result: It shows the Patient's name, alert type, time of the alert, provider's name,
+--          and any associated notes for monitoring purposes.
+
+SELECT
+    PATIENTS.First_name AS Patient_First_Name,
+    PATIENTS.Last_name AS Patient_Last_Name,
+    ALERTS.Alert_type AS Alert_Type,
+    ALERTS.Time_stamp AS Alert_Time,
+    PROVIDERS.First_name AS Provider_First_Name,
+    PROVIDERS.Last_name AS Provider_Last_Name,
+    HEALTH_SUMMARY.Provider_notes AS Provider_Notes
+FROM
+    PATIENTS
+        JOIN
+    ALERTS ON PATIENTS.Patient_ID = ALERTS.Patient_ID
+        JOIN
+    HEALTH_SUMMARY ON PATIENTS.Patient_ID = HEALTH_SUMMARY.Patient_ID
+        JOIN
+    PROVIDERS ON HEALTH_SUMMARY.Provider_ID = PROVIDERS.Provider_ID
+WHERE
+    ALERTS.Resolved = 'F';
+
+
